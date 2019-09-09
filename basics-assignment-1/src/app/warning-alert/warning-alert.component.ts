544cd9e26f437c556c0740cb8f7eff2a197b4344
cdsalty <soltiscd@gmail.com>
2019-09-08 22:25:09 -0400

    updating assignment to work correctly

---
+++ basics-assignment-1/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "app-warning-alert",
+  template: `
+    <p>Warning!! You are in danger</p>
+  `
+})
+
+export class WarningAlertComponent {
+
+}
+
+
+//Step 1: export Class
+//Step 2: import Component
+//Step 3: @Component ({})
